be099870d9dc6e327e729c90bcba4e47ab745d17
Zhong <15089210041@163.com>
2021-11-14 13:25:40 +0800

    conflict test

--- src/main/java/com/zyy/MyTest.java
+++ src/main/java/com/zyy/MyTest.java
@@ -3,6 +3,8 @@ package com.zyy;
 public class MyTest {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("2");
         System.out.println("pull test");
+        System.out.println("conflict test");
     }
 }
